ba3f5ef85373b05824473aa49e5aa506804299f4
Angus Dale <22398@student.mcc.act.edu.au>
2017-10-16 08:50:16 +1100

    Initial Commit

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * 8, Input.GetAxis("Vertical") * Time.deltaTime * 8);
+        transform.Translate(input);
+    }
+}
