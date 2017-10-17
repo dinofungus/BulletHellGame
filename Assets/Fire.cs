c3cf2976544b0a4bb495414071cb6d332aeefd13
Angus Dale <22398@student.mcc.act.edu.au>
2017-10-17 14:21:49 +1100

    add basic bullet code

---
+++ Assets/Fire.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed;
+    void Start () {
+		
+	}
+    
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0, 1 * speed * Time.deltaTime));
+	}
+}
