cd92acf6938a7bc0f4b6ec626bdf9141a0e54034
Angus Dale <22398@student.mcc.act.edu.au>
2017-10-19 10:19:34 +1100

    Player fires bullets now

---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public float fireRate;
+	public GameObject shot;
+	public Transform shotSpawn;
+
+    private float nextShot = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nextShot += Time.deltaTime;
+        if (nextShot >= fireRate && Input.GetButton("Fire1"))
+        {
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            nextShot = 0;
+        }
+	}
+}
